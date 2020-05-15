1590b01e930d6120eb55274ec9baeb0810f5d23c
Tom Hamson <thomas.hamson@hamsoncreative.co.uk>
2020-05-15 18:52:27 +0100

    Replaced reset results button with automatic reset results after 2s.

--- app.js
+++ app.js
@@ -6,19 +6,41 @@ const computerChoice = document.getElementById('computer-choice')
 const randomNumber = Math.round(Math.random() * (3))
 const userChoice = document.getElementById('user-choice')
 const possibleChoices = document.querySelectorAll('.choices')
-const reset = document.getElementById('reset')
 const resetGame = document.getElementById('reset-game')
 var won = document.getElementById('won')
 var lost = document.getElementById('lost')
 var wonCounter = isNaN(parseInt(won)) ? 0 : parseInt(won)
 var lostCounter = isNaN(parseInt(lost)) ? 0 : parseInt(lost)
 
+// Update score
+function updateScore(result) {
+  if (result === 'you win') {
+    wonCounter++
+    won.innerHTML = wonCounter.toString()
+  } else if (result === 'you lost') {
+    lostCounter++
+    lost.innerHTML = lostCounter.toString()
+  }
+}
+
+// Reset game data
+function resetGameData() {
+  wonCounter = 0
+  won.innerHTML = wonCounter.toString()
+  lostCounter = 0
+  lost.innerHTML = lostCounter.toString()
+  resetResults()
+}
+
+// Reset results
+function resetResults() {
+  userChoice.innerHTML = ''
+  computerChoice.innerHTML = ''
+  displayResult.innerHTML = ''
+}
+
 resetGame.addEventListener('click', (e) => {
   resetGameData()
-})
-
-reset.addEventListener('click', (e) => {
-  resetResults()
 })
 
 // Get userChosen
@@ -30,6 +52,7 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
   userChoice.innerHTML = userChosen
   computerChoice.innerHTML = computerChosen
   displayResult.innerHTML = result
+  setTimeout(resetResults, 2000)
 }))
 
 // Get a random computer choice
@@ -60,31 +83,4 @@ function results() {
   } else if (computerChosen === 'scissors' && userChosen === 'paper') {
     return result = 'you lost'
   }
-}
-
-// Reset results
-function resetResults() {
-  userChoice.innerHTML = ''
-  computerChoice.innerHTML = ''
-  displayResult.innerHTML = ''
-}
-
-// Update score
-function updateScore(result) {
-  if (result === 'you win') {
-    wonCounter++
-    won.innerHTML = wonCounter.toString()
-  } else if (result === 'you lost') {
-    lostCounter++
-    lost.innerHTML = lostCounter.toString()
-  }
-}
-
-// Reset results
-function resetGameData() {
-  wonCounter = 0
-  won.innerHTML = wonCounter.toString()
-  lostCounter = 0
-  lost.innerHTML = lostCounter.toString()
-  resetResults()
 }
